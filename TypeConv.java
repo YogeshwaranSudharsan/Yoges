97faa076295c5ec2f62708836f525a71bd4680a9
YogeshwaranSudharsan <yogeshwaran@gmail.com>
2018-05-29 12:37:13 -0700

    Adding

---
+++ TypeConv.java
@@ -0,0 +1,9 @@
+public class TypeConv{
+public static void main(String s[]){
+System.out.println("Type Conversion");
+byte one = 10;
+byte two = 20;
+int three = one + two;
+System.out.println(three);
+}
+}
